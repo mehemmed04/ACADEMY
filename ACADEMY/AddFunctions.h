8d9a6db4b5311c8245e605714b76f983a1c51b07
mehemmedbayramov <mehemmedbayramov2004@gmail.com>
2022-03-10 20:12:45 +0400

    I add Add Functions.

---
+++ ACADEMY/AddFunctions.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<iostream>
+#include"Entities.h"
+using namespace std;
+
+Teacher* GetNewTeacher() {
+	cout << "Enter Teacher fullname : ";
+	cin.ignore();
+	cin.clear();
+	char* fullname = new char[100]{};
+	cin.getline(fullname,100);
+	
+	cout << "Enter Teacher occupation : ";
+	char* occupation = new char[100]{};
+	cin.getline(occupation, 100);
+
+	auto teacher = new Teacher{ fullname,occupation };
+	return teacher;
+}
+
+void AddTeacher(Academy* academy ,Teacher** teachers) {
+	auto teacher = GetNewTeacher();
+	int count = academy->teacher_count;
+	auto newteachers = new Teacher*[count + 1];
+
+	for (size_t i = 0; i < count; i++)
+	{
+		newteachers[i] = teachers[i];
+	}
+	newteachers[count] = teacher;
+	teachers = newteachers;
+	newteachers = nullptr;
+	academy->teacher_count++;
+}
+
